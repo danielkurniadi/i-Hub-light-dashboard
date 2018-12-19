d8e3f56b64f88b3cfa34fcb2c20467e2c0984353
iqDF <daniel.thekurniadi@gmail.com>
2018-12-19 17:57:06 +0800

    implement post/ insert button

--- testing/client-test.js
+++ testing/client-test.js
@@ -15,12 +15,12 @@ function TableContructor(){
         this.row_id = 1;
 
         // append and display html content
-        this.construct = (data)=>{
+        this.construct = (is_header=false, data)=>{
             console.log(data);
             
             // parse header
             let header = `<th>#</th>`;
-            Object.keys(data[0]).forEach(fieldName => header += `<th scope="col">${fieldName}</th>`);
+            if(is_header){ Object.keys(data[0]).forEach(fieldName => header += `<th scope="col">${fieldName}</th>`); }
 
             // parse content
             let content = data.reduce((rowStacks, rawData)=>{
@@ -40,21 +40,25 @@ function TableContructor(){
 
 const tableConstructor = new TableContructor();
 
-function getProfiles(conditions){
+function getProfiles(conditions, n_limit=10000){
     // parse condition
-
+    let query_cond;
+    if(conditions){ query_cond = ` WHERE ${conditions.col} \'${conditions.value}\'`; }
+    else { query_cond = '';}
+    // parse limit amount
+    let query_limit = ` LIMIT ${n_limit}`;
     // call API
     blockspring.runParsed("serversideapi", 
-        { 
-            "method": 'GET', 
-            "doc_name": CONSTANTS.STUDENTS_TABLE.TABLE_NAME, 
-            "worksheet_id": CONSTANTS.STUDENTS_TABLE.WID[0],
-            "query": "SELECT B, C, D, E, F" //"SELECT B, C, D, E, F WHERE C CONTAINS"  + matric
-        }, 
-        function(res){
-            // return header and content in tabular form
-            return tableContructor.construct(res.params.data);
-        
+            { 
+                "method": 'GET', 
+                "doc_name": CONSTANTS.STUDENTS_TABLE.TABLE_NAME, 
+                "worksheet_id": CONSTANTS.STUDENTS_TABLE.WID[0],
+                "query": "SELECT B, C, D, E, F" + query_cond + query_limit //"SELECT B, C, D, E, F WHERE C CONTAINS"  + matric
+            }, 
+            function(res){
+                // return header and content in tabular form
+                // return tableContructor.construct(res.params.data);
+                return res.params.data;
     });   
 };
 
@@ -69,8 +73,9 @@ function postAttendance(values){
             },
             function(res){
                 // return status
+                console.log(res.params);
                 return res.params;
-            });
+    });
 };
 
 
